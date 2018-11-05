c900b1505727f7de7043f69b5edbec31b2704eb6
gokul2594 <gokul2594@gmail.com>
2018-11-05 16:14:20 -0500

    Cart functionality and view added and remove cart button and functionality added

--- MusicStore/Controllers/StoreController.cs
+++ MusicStore/Controllers/StoreController.cs
@@ -10,10 +10,12 @@ namespace MusicStore.Controllers
 {
     public class StoreController : Controller
     {
+        MusicStoreModel db = new MusicStoreModel();
         // GET: Store
         public ActionResult Index()
         {
-            return View();
+            var genres = db.Genres.OrderBy(g => g.Name).ToList();
+            return View(genres);
         }
 
         public ActionResult Product(string ProductName)
@@ -23,17 +25,80 @@ namespace MusicStore.Controllers
         }
 
         //Get: store/Albums
-        public ActionResult Albums()
+        public ActionResult Albums(String genre)
         {
             //mock up some album data
-            var albums = new List<Album>();
+            //var albums = new List<Album>();
 
-            for (int i=1; i<10; i++)
-            {
-                albums.Add(new Album { Title = "Album" + i.ToString() });
-            }
-            
+            //            for (int i=1; i<10; i++)
+            //          {
+            //            albums.Add(new Album { Title = "Album" + i.ToString() });
+            //      }
+            var albums = db.Albums.Where(a => a.Genre.Name == genre).OrderBy(a => a.Title).ToList();
+            ViewBag.Genre = genre;
             return View(albums);
+        }
+
+        //Get: Store/AddToCart/5
+        public ActionResult AddToCart(int AlbumId)
+        {
+            GetCartId();
+            string CurrentCartId = Session["CartId"].ToString(); 
+            Cart cartItem = new Cart
+            {
+                AlbumId = AlbumId,
+                Count = 1,
+                DateCreated = DateTime.Now,
+                CartId = CurrentCartId
+            };
+
+            db.Carts.Add(cartItem);
+            db.SaveChanges();
+
+            //show cart page
+            return RedirectToAction("ShoppingCart");
+        }
+
+        private void GetCartId()
+        {
+            //is there already a cart id
+            if(Session["CartId"] == null)
+            {
+                //is user loggedin?
+                if (User.Identity.Name == "") {
+                    //generate unique id that is session specific
+                    Session["CartId"] = Guid.NewGuid().ToString();
+                }
+                //if user is not logged in
+                else
+                {
+                    Session["CartId"] = User.Identity.Name;
+                }
+            }
+        }
+
+        //GET: Store/ShoppingCart
+
+        public ActionResult ShoppingCart()
+        {
+            //get current Cart for display
+            GetCartId();
+            string CurrentCartId = Session["CartId"].ToString();
+            var cartItems = db.Carts.Where(c => c.CartId == CurrentCartId);
+            //load view and pass the list of items in the users cart
+            return View(cartItems);
+        }
+
+        //GET: Store/RemoveFromCart/4
+        public ActionResult RemoveFromCart(int RecordId)
+        {
+            //remove item from users cart
+            Cart CartItem = db.Carts.SingleOrDefault(c => c.RecordId == RecordId);
+            db.Carts.Remove(CartItem);
+            db.SaveChanges();
+
+            //reload cart page
+            return RedirectToAction("ShoppingCart");
         }
     }
 }
